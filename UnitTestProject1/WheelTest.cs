77c121a334bc496bd3d802eb8d254a6666aa129c
Nazgul07 <colton.c.correll@gmail.com>
2015-11-24 19:54:23 -0600

    Initial File commit

---
+++ UnitTestProject1/WheelTest.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wheel;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class WheelTest
+	{
+		private static Wheel<int> Setup()
+		{
+			var wheel = new Wheel<int>();
+			wheel.Push(1);
+			wheel.Push(2);
+			wheel.Push(3);
+			return wheel;
+		}
+
+		[TestMethod]
+		public void TestCount()
+		{
+			var wheel = Setup();
+			Assert.AreEqual(3, wheel.Count);
+		}
+
+		[TestMethod]
+		public void TestPeek()
+		{
+			var wheel = Setup();
+			Assert.AreEqual(3, wheel.Peek());
+		}
+
+		[TestMethod]
+		public void TestPop()
+		{
+			var wheel = Setup();
+			Assert.AreEqual(3, wheel.Pop());
+			Assert.AreEqual(2, wheel.Pop());
+		}
+
+		[TestMethod]
+		public void TestRotate()
+		{
+			var wheel = Setup();
+			wheel.Rotate(1);
+			Assert.AreEqual(1, wheel.Pop());
+			Assert.AreEqual(3, wheel.Pop());
+		}
+
+		[TestMethod]
+		public void TestRotate2()
+		{
+			var wheel = Setup();
+			wheel.Rotate(4);
+			Assert.AreEqual(1, wheel.Pop());
+			Assert.AreEqual(3, wheel.Pop());
+		}
+
+		[TestMethod]
+		public void TestRotate3()
+		{
+			var wheel = Setup();
+			wheel.Rotate(7);
+			Assert.AreEqual(1, wheel.Pop());
+			Assert.AreEqual(3, wheel.Pop());
+		}
+
+		[TestMethod]
+		public void TestRotate4()
+		{
+			var wheel = Setup();
+			wheel.Rotate(2);
+			Assert.AreEqual(2, wheel.Pop());
+			Assert.AreEqual(1, wheel.Pop());
+		}
+
+		[TestMethod]
+		public void TestRotate5()
+		{
+			var wheel = Setup();
+			wheel.Rotate(3);
+			Assert.AreEqual(3, wheel.Pop());
+			Assert.AreEqual(2, wheel.Pop());
+		}
+	}
+}
---
+++ Wheel/Wheel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Wheel
+{
+    public class Wheel<T> : IEnumerable<T>, IEnumerable, IReadOnlyCollection<T>
+	{
+		public int Count {
+			get
+			{
+				return _position + 1;
+			}
+		}
+
+		private T[] _items;
+
+		private int _position;
+
+		public Wheel() : this(10)
+		{
+
+		}
+
+		public Wheel(int initialSize)
+		{
+			_items = new T[initialSize];
+			_position = -1;
+        }
+
+		public T Push(T item)
+		{
+			if (_position + 1 == _items.Length)
+			{
+				Array.Resize(ref _items, _items.Length * 2);
+			}
+			if (_position + 1 < _items.Length)
+			{
+                _items[++_position] = item;
+			}
+			return item;
+		}
+
+		public T Peek()
+		{
+			if (_position == -1)
+				throw new IndexOutOfRangeException();
+			return _items[_position];
+		}
+
+		public T Pop()
+		{
+			if (_position == -1)
+				throw new IndexOutOfRangeException();
+			T item = _items[_position];
+			_items[_position--] = default(T);
+			return item;
+		}
+
+		public void Rotate(int positions)
+		{
+			int newPosition = _position;
+			newPosition += positions;
+			while(newPosition > _position)
+			{
+				newPosition -= _position;
+				newPosition--;
+			}
+			if(newPosition < _position)
+			{
+				T[] leftArray = new T[newPosition + 1];
+				Array.Copy(_items, leftArray, newPosition + 1);
+				T[] rightArray = new T[_position - newPosition];
+				Array.Copy(_items, newPosition + 1, rightArray, 0, rightArray.Length);
+				T[] newArray = new T[_items.Length];
+				Array.Copy(rightArray, 0, newArray, 0, rightArray.Length);
+				Array.Copy(leftArray, 0, newArray, rightArray.Length, leftArray.Length);
+				_items = newArray;
+			}
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return _items.GetEnumerator();
+		}
+		
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			for(int i = 0; i <= _position; i++)
+			{
+				yield return _items[i];
+			}
+		}
+	}
+}
